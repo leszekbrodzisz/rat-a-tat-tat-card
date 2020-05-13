5466070da68e217987701536e5b95071e1b6d56b
Leszek Brodzisz <Leszek.Brodzisz@gmail.com>
2020-05-13 06:31:42 +0200

    commit9

--- rat.py
+++ rat.py
@@ -1,41 +1,40 @@
 from flask import (Flask, render_template, abort, jsonify, request, redirect, url_for)
-
-
-players = {
-  "player1": "P1",
-  "player2": "P2",
-  "player3": "P3",
-  "player4": "P4",
-}
-
-results = {
-  "player1": 1,
-  "player2": 0,
-  "player3": 0,
-  "player4": 0,
-}
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired
+#from babel import Babel
 
 app = Flask(__name__)
+#babel = Babel(app)
+app.config['SECRET_KEY'] = 'sekretny_klucz'
+
+
+class UsersNumber(FlaskForm):
+    WTF_I18N_ENABLED = True
+    class Meta:
+        #locales = ['pl_PL', 'pl']
+        #WTF_I18N_ENABLED ustawic na false???
+
+        locales = ['es_ES', 'es']
+    how_many_players = IntegerField('lableka how_many_players', validators=[InputRequired(message='Podanie liczby graczy jest konmiczene')])
+
+
 
 @app.route('/', methods=["GET", "POST"])
-def velcome():
-    if request.method == "POST":
-        #something will come here
-        players["player1"] = request.form["form_p1_name"]
-        players["player2"] = request.form["form_p2_name"]
-        players["player3"] = request.form["form_p3_name"]
-        players["player4"] = request.form["form_p4_name"]
+def welocome():
+    form = UsersNumber()
+    if form.validate_on_submit():
+        return 'jest przycisk'
 
-        return (redirect(url_for('tours_run', players=players, results=results)))
-    else:
-        return render_template("welcome.html", text1 = "Wstawiony text RRRRR")
+    return render_template("welcome.html", form=form)
 
 @app.route('/tours_run', methods=["GET", "POST"])
 def tours_run():
     if request.method == "POST":
         #something will come here
-        results["player1"] = 1
+        a=1
+
 
         #return (redirect(url_for('tours_run', players=players, results=results)))
     else:
-        return render_template("tours_run.html", players = players, results = results )
+        return render_template("tours_run.html")
